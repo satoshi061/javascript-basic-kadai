65d7b0d206ad5856a59b9a352baf8e08d5d769c1
satoshi061 <shankou0302@gmail.com>
2024-07-24 20:50:45 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const getDate = () => {
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+  console.log(year+"年"+month+"月"+date+"日");
+}
+
+getDate();
