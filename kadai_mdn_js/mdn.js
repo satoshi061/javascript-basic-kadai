01fe4a9afdd6ebb8914aa2b377455250aa24ceff
satoshi061 <shankou0302@gmail.com>
2024-07-24 20:50:25 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const getDate = () => {
-  let today = new Date();
-  let year = today.getFullYear();
-  let month = today.getMonth();
-  let date = today.getDate();
-  console.log(year+"年"+month+"月"+date+"日");
-}
-
-getDate();
